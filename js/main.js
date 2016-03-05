660026859e53d32847041fe17a7aca1dbc3c371f
Bharath Gedela <bgbharath722@gmail.com>
2016-03-05 16:18:40 +0530

    Chiku front-end files

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+$('.nav li a').click(function(chiku) {
+  var $this = $(this);
+  if (!$this.hasClass('active')) {
+    $this.addClass('active');
+  }
+  chiku.preventDefault();
+});
+
+ $(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
